1a39872ac080d5387b3edd4eaafe32fe7efc3ca8
F6322043 Lucas Duarte Pinheiro <luc.dpinheiro@bb.com.br>
2022-09-30 12:14:25 -0300

    Edit: Script sql para criação do banco de dados removido. Adicionada configuração em applicaton.properties para criação do db automática pelo hibernate. Modificação do caminho do db para o hibernate.

--- src/main/java/br/com/saude/models/Goals.java
+++ src/main/java/br/com/saude/models/Goals.java
@@ -1,8 +1,13 @@
 package br.com.saude.models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -11,9 +16,16 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name="saude", schema="goals")
-public class Goals {
+@Table(name="goals")
+public class Goals  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable=false)
     private int hydration = 0;
+
+    @Column(nullable=false)
     private BigDecimal weightLoss = new BigDecimal(0);
 }
